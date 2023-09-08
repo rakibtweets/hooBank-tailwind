dc24621dcd67f2a1945df63bda25b80b8078a4e3
Rakib Hasan <rakibhasanroky0@gmail.com>
2023-09-08 23:20:24 +0600

    initial setup

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <h1 className="text-4xl text-x font-bold text-center text-green-600">
+      HooBand Tailwind Project
+    </h1>
+  );
+}
+
+export default App;
